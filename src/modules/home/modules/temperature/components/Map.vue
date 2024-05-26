a7ad567b60c37997cbaa06419333cf8f1211750b
Antony Ayansi <toileok1@gmail.com>
2024-05-26 18:10:03 -0500

    feat: Mejorar componente Map.vue y agregar funcionalidad de ubicación
    - Mejorar el componente Map.vue para mostrar un mapa interactivo de temperatura.
    - Mostrar un marcador en la ubicación del usuario en el mapa.

--- src/modules/home/modules/temperature/components/Map.vue
+++ src/modules/home/modules/temperature/components/Map.vue
@@ -1,37 +1,53 @@
-<template>
-    <h1>Map</h1>    
-    <div class="w-full h-52" id="map"></div>
+<template> 
+  <div class="w-full h-52 rounded-xl" id="map"></div>
 </template>
-  
+
 <script setup>
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
+
+const isLocationEnabled = ref(false);
+
+const captureLatLong = (map) => {
+  navigator.geolocation.getCurrentPosition(
+      (position) => {
+          const { latitude, longitude } = position.coords;
+          isLocationEnabled.value = true;
+          // Centra el mapa en las coordenadas del usuario.
+          map.setCenter([longitude, latitude]);
+
+          // Crea un marcador en la ubicación del usuario.
+          new mapboxgl.Marker()
+              .setLngLat([longitude, latitude])
+              .addTo(map);
+      },
+      (error) => {
+          console.error("Error al obtener la ubicación", error);
+          // alert("No se pudo obtener la ubicación. Asegúrate de que los servicios de ubicación estén habilitados y que hayas concedido permisos.");
+          map.setCenter([-13.5364608, -71.9519744])
+      }
+  );
+}
 
 onMounted(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYW50b255d3lhdHQiLCJhIjoiY2xyZmFmdmN2MDMyYjJrcm1uMHgwZ3ZjbiJ9.49lhOvUiuPu0R38iSmVv2w'
-    const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/light-v9",
-    });
-    map.on("load", () => {
-        // Here we want to load a layer
-        map.addSource("usa", {
-          type: "geojson",
-          data:
-            "https://raw.githubusercontent.com/johan/world.geo.json/master/countries/USA.geo.json",
-        });
-        map.addLayer({
-          id: "usa-fill",
-          type: "fill",
-          source: "usa",
-          paint: {
-            "fill-color": "red",
-          },
-        });
-    });
-})
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYW50b255d3lhdHQiLCJhIjoiY2xyZmFmdmN2MDMyYjJrcm1uMHgwZ3ZjbiJ9.49lhOvUiuPu0R38iSmVv2w';
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    zoom: 13, // starting zoom
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0], // Centro inicial, se actualizará con la ubicación del usuario
+  });
+
+  captureLatLong(map);
+});
 </script>
 
 <style scoped>
+#w-full {
+  width: 100%;
+}
+#h-52 {
+  height: 52vh;
+}
 </style>
