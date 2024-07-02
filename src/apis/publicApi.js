3f0ce2e481362471f19e7c1574cb405f4b933728
Antony <antonyayansi@gmail.com>
2024-07-02 14:18:27 -0500

    Cambio de servidor

--- src/apis/publicApi.js
+++ src/apis/publicApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const publicApi = axios.create({
     /** Ruta base de la API, funciona siempre y cuando el usuario este logueado */
-    baseURL: 'https://ecox.ccore.store/api',
+    baseURL: 'https://ecoexplorer.000webhostapp.com/api',
     //baseURL: 'http://localhost:9002/api',
 })
 
