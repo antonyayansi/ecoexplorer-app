161c5f2c82e427c348dcfe503e8669b9f7252920
Antony <antonyayansi@gmail.com>
2024-08-23 12:35:58 -0500

    Cambio de servidor

--- src/apis/publicApi.js
+++ src/apis/publicApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const publicApi = axios.create({
     /** Ruta base de la API, funciona siempre y cuando el usuario este logueado */
-    baseURL: 'https://ecoexplorer.000webhostapp.com/api',
+    baseURL: 'https://ecox.credisur-peru.com/api',
     //baseURL: 'http://localhost:9002/api',
 })
 
